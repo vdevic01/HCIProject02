a1f9c425da224f2c3ecedf082fae147ba46cea45
Veljko Bubnjevic <96338927+bubnjevich@users.noreply.github.com>
2023-06-07 15:39:40 +0200

    info attraction view

---
+++ HCIProject02/GUI/Features/ClientInterface/Attractions/InfoAttractionView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HCIProject02.GUI.Features.ClientInterface.Attractions
+{
+    /// <summary>
+    /// Interaction logic for InfoAttractionView.xaml
+    /// </summary>
+    public partial class InfoAttractionView : UserControl
+    {
+        public InfoAttractionView()
+        {
+            InitializeComponent();
+        }
+    }
+}
